0f4ac7dbb2135f09cd5eedb3f0aa721996408858
maddie-li <115739216+maddie-li@users.noreply.github.com>
2025-07-13 11:11:59 -0700

    Sets mass from radius and local gravity
    This will help calculate orbits later and is easier to tweak planet properties

--- Assets/Scripts/Physics/GravityBody.cs
+++ Assets/Scripts/Physics/GravityBody.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class GravityBody : MonoBehaviour
 {
+    [Header("Gravity Settings")]
     [HideInInspector] public Rigidbody rb;
     public bool isGravitySource;
+    public float localGravity;
+    public float radius;
+
+    [Header("Orbit Settings")]
+    public GravityBody orbitTarget;
     public Vector3 initialVelocity;
 
     //public Vector3 velocity;
+
+    private void OnValidate()
+    {
+        UpdateBodySize();
+    }
 
     private void Start()
     {
@@ -16,9 +27,27 @@ public class GravityBody : MonoBehaviour
         rb.isKinematic = isGravitySource;
         rb.useGravity = false;
 
-        rb.velocity = initialVelocity;
-
         GravityManager.Instance.RegisterBody(this);
+
+        // set size
+        UpdateBodySize();
+
+        if (gameObject.CompareTag("Player")) return;
+
+        /*// set mass using local gravity and radius
+        localGravity = localGravity == 0 ? 1f : localGravity;
+        rb.mass = (localGravity * radius * radius) / GravityManager.Instance.gravitationalConstant;*/
+
+        if (isGravitySource) return;
+
+        rb.velocity = initialVelocity;
+    }
+
+    private void UpdateBodySize()
+    {
+        // set size using radius
+        radius = radius == 0 ? 1f : radius;
+        transform.localScale = Vector3.one * radius * 2;
     }
 
     private void OnDestroy()
@@ -26,6 +55,8 @@ public class GravityBody : MonoBehaviour
         if (GravityManager.Instance != null)
             GravityManager.Instance.UnregisterBody(this);
     }
+
+
     
     /*
 
