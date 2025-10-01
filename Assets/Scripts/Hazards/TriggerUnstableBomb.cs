28e1220aaed88cab1d11cced36b0af39c764d086
maddie-li <115739216+maddie-li@users.noreply.github.com>
2025-09-30 20:30:19 -0700

    Unstable Bombs can be marked OnlyAffectPlayer

--- Assets/Scripts/Hazards/TriggerUnstableBomb.cs
+++ Assets/Scripts/Hazards/TriggerUnstableBomb.cs
@@ -6,21 +6,31 @@ public class TriggerUnstableBomb : MonoBehaviour
 {
     private UnstableBomb bomb;
 
-   public void Init(UnstableBomb parentBomb)
-   {
-        bomb = parentBomb;
-   }
+    public void Init(UnstableBomb parentBomb)
+    {
+         bomb = parentBomb;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("CollisionIgnore")) return;
+        if (!DetectCollision(other)) return;
+
+        Debug.Log(other + " Entered");
         bomb.HandleTriggerEnter(other);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("CollisionIgnore")) return;
+        if (!DetectCollision(other)) return;
+
         Debug.Log(other + " Exited");
         bomb.HandleTriggerExit(other);
+    }
+
+    private bool DetectCollision(Collider other)
+    {
+        if (other.CompareTag("CollisionIgnore")) return false;
+        else if (!other.CompareTag("Player") && bomb.OnlyAffectsPlayer) return false;
+        else return true;
     }
 }
