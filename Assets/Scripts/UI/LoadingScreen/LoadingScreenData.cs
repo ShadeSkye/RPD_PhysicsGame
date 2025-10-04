92bcc7cd0ee2921766bbce9796bfa55cc32f2be4
maddie-li <115739216+maddie-li@users.noreply.github.com>
2025-10-04 01:54:45 -0700

    Created loading screens (and also new ship SO)

---
+++ Assets/Scripts/UI/LoadingScreen/LoadingScreenData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Level/Loading Screen")]
+public class LoadingScreenData : ScriptableObject
+{
+    [Header("Visuals")]
+    public Sprite backgroundImage;
+    public string loadingText = "Loading...";
+
+    [Header("Options")]
+    public float holdTime = 2f;
+    public bool requireContinueButton = true;
+}
+
