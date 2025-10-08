3ddd693c04bf3d561602fa3f89d7266eb7cba475
Amelia Nichols <amelia6nichols@gmail.com>
2025-10-08 01:56:41 -0700

    wrote loading screen text

--- Assets/Scripts/UI/LoadingScreen/LoadingScreenData.cs
+++ Assets/Scripts/UI/LoadingScreen/LoadingScreenData.cs
@@ -7,7 +7,7 @@ public class LoadingScreenData : ScriptableObject
 {
     [Header("Visuals")]
     public Sprite backgroundImage;
-    public string loadingText = "Loading...";
+    [TextArea] public string loadingText = "Loading...";
 
     [Header("Options")]
     public float holdTime = 2f;
